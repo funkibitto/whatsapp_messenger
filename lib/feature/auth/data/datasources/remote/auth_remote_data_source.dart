80ce1d407f9bdd2f881bdd58517d74f7d9cdf3ce
kitae-yun <kitae.yun@gwabba.com>
2023-02-25 08:31:38 +0900

    login page

--- lib/feature/auth/data/datasources/remote/auth_remote_data_source.dart
+++ lib/feature/auth/data/datasources/remote/auth_remote_data_source.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:whatsapp_messenger/feature/auth/data/models/user_model.dart';
 
 abstract class AuthRemoteDataSource {
-  Stream<UserModel> getUserPresenceStatus();
-  Future<void> setAppLocale(String languageCode);
+  Stream<UserModel> getUserPresenceStatus(String uid);
+
+  Future<UserModel?> getUserInfo();
+
+  Future<String> uploadProfileImage(File imageFile);
+
+  Future<void> createUser(
+      {required String username, required String phoneNumber});
 }
